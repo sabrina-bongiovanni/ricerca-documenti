a09a325573418d476c33a6595cb7762d833b175a
Sabrina Bongiovanni <sabrina.bongiovanni@redturtle.it>
2022-11-02 09:13:24 +0100

    first commit

---
+++ node_modules/design-react-kit/src/Notification/index.ts
@@ -0,0 +1,9 @@
+/* @internal */
+export { createNotification } from './NotificationContent';
+export { NotificationManager } from './NotificationManager';
+export { notify } from './core';
+
+export type { NotificationProps } from './Notification';
+export type { NotificationManagerProps } from './NotificationManager';
+export type { NotificationOptions } from './NotificationContent';
+export type { NotificationId } from './types';
